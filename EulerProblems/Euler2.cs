9948892d81d17b36a396a833ff90e9b78b036aa8
Somnuk Wongkhan <somnuk.wk@gmail.com>
2014-12-23 13:56:13 +0700

    euler 2

---
+++ EulerProblems/Euler2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using NUnit.Framework;
+
+namespace EulerProblems
+{
+	[TestFixture]
+	public class Euler2
+	{
+		[Test]
+		public void TestCaseI()
+		{
+			var max = 4000000;
+			var start = 1;
+			var next = 1;
+			var sum = 0;
+			while (next < max) {
+				if (next % 2 == 0)
+					sum += next;
+				var temp = start + next;
+				start = next;
+				next = temp;
+			}
+			Assert.AreEqual (4613732, sum);
+		}
+	}
+}
